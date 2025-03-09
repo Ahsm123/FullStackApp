8c0d27ff95831626af012d25409018cfab2992c0
Ahsm123 <10503096@ucn.dk>
2025-03-09 16:52:43 +0100

    Initial Commit with frontend

---
+++ frontend/src/temp-data.js
@@ -0,0 +1,45 @@
+import caputoBlue from "@/assets/caputo-blue.webp";
+import caputoRed from "@/assets/caputo-red.webp";
+import caputoYellow from "@/assets/caputo-yellow.webp";
+
+export const cartItems = [
+  {
+    id: "1",
+    name: "Caputo Blå",
+    price: "DKK 20.00",
+    imageName: caputoBlue,
+  },
+  {
+    id: "2",
+    name: "Caputo Rød",
+    price: "DKK 20.00",
+    imageName: caputoRed,
+  },
+  {
+    id: "3",
+    name: "Caputo Gul",
+    price: "DKK 20.00",
+    imageName: caputoYellow,
+  },
+];
+
+export const products = [
+  {
+    id: "1",
+    name: "Caputo Blå",
+    price: "DKK 20.00",
+    imageName: caputoBlue,
+  },
+  {
+    id: "2",
+    name: "Caputo Rød",
+    price: "DKK 20.00",
+    imageName: caputoRed,
+  },
+  {
+    id: "3",
+    name: "Caputo Gul",
+    price: "DKK 20.00",
+    imageName: caputoYellow,
+  },
+];
