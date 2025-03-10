8c2faec5c2b6ce89cffface2cb9f391eb3af31d6
Ahsm123 <10503096@ucn.dk>
2025-03-10 21:32:49 +0100

    added endpoints for backend

---
+++ backend/src/temp-data.js
@@ -0,0 +1,32 @@
+// import caputoBlue from "@/assets/caputo-blue.webp";
+// import caputoRed from "@/assets/caputo-red.webp";
+// import caputoYellow from "@/assets/caputo-yellow.webp";
+
+export const cartItems = ["1", "2", "3", "4"];
+
+export const products = [
+  {
+    id: "1",
+    name: "Caputo Blå",
+    price: "DKK 20.00",
+    // imageName: caputoBlue,
+  },
+  {
+    id: "2",
+    name: "Caputo Rød",
+    price: "DKK 20.00",
+    // imageName: caputoRed,
+  },
+  {
+    id: "3",
+    name: "Caputo Gul",
+    price: "DKK 20.00",
+    // imageName: caputoYellow,
+  },
+  {
+    id: "4",
+    name: "Caputo Lilla",
+    price: "DKK 20.00",
+    // imageName: caputoYellow,
+  },
+];
